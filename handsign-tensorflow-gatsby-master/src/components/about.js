c1097bb3dbc23b1b3f22664fa674f0ec0b7dc094
Mayur Thakkar <36958377+MThakkar121@users.noreply.github.com>
2021-03-14 16:25:40 +0530

    Update about.js

--- handsign-tensorflow-gatsby-master/src/components/about.js
+++ handsign-tensorflow-gatsby-master/src/components/about.js
@@ -25,13 +25,13 @@ export default function About() {
             <Modal onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>American Sign Language (ASL)</ModalHeader>
+          <ModalHeader>Indian Sign Language (ISL)</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-              <Text fontSize="sm">American Sign Language (ASL) is a visual language that serves as the predominant sign language of Deaf communities in the United States and most of Canada.<br></br>
-          Here's the list of ASL hand gestures for alphabet.</Text>
+              <Text fontSize="sm">Indian Sign Language (ISL) is a visual language that serves as the predominant sign language of Deaf communities in the India.<br></br>
+          Here's the list of ISL hand gestures for alphabet.</Text>
           <Image src={handImages}/>
-          <Text fontSize="sm">This sign language illustration is created by <Link href="https://thenounproject.com/pelodrome/" isExternal color="orange.300">Pelin Kahraman</Link></Text>
+          //<Text fontSize="sm">This sign language illustration is created by <Link href="https://thenounproject.com/pelodrome/" isExternal color="orange.300">Pelin Kahraman</Link></Text>
             
           </ModalBody>
           <ModalFooter>
